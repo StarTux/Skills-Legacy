6ab41e570dcbb7eedfba56744b26ae2918d82773
Thomas Loy <ths.loy@googlemail.com>
2013-11-23 09:16:33 +0100

    Make anvil repairs and combinations count for the enchanting skill

--- src/com/winthier/skills/skill/EnchantingSkill.java
+++ src/com/winthier/skills/skill/EnchantingSkill.java
@@ -4,12 +4,18 @@ import com.winthier.skills.SkillsPlugin;
 import com.winthier.skills.util.MaterialFractionMap;
 import java.util.ArrayList;
 import java.util.List;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class EnchantingSkill extends AbstractSkill {
         private MaterialFractionMap itemSpMap = new MaterialFractionMap(0);
@@ -27,14 +33,53 @@ public class EnchantingSkill extends AbstractSkill {
                 return Math.min(50, 30 + level / 20);
         }
 
-        @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+        @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
         public void onEnchantItem(EnchantItemEvent event) {
                 final Player player = event.getEnchanter();
                 final int xpLevels = event.getExpLevelCost();
                 final ItemStack item = event.getItem();
+                onEnchant(player, item.getType(), xpLevels);
+        }
 
+        @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+        public void onInventoryClick(InventoryClickEvent event) {
+                if (event.getInventory().getType() != InventoryType.ANVIL) return;
+                if (event.getSlotType() != InventoryType.SlotType.RESULT) return;
+                switch (event.getAction()) {
+                case DROP_ALL_SLOT:
+                case DROP_ONE_SLOT:
+                case HOTBAR_MOVE_AND_READD:
+                case HOTBAR_SWAP:
+                case MOVE_TO_OTHER_INVENTORY:
+                case PICKUP_ALL:
+                case PICKUP_HALF:
+                case PICKUP_ONE:
+                case PICKUP_SOME:
+                        break;
+                default:
+                        return;
+                }
+                final Inventory inv = event.getInventory();
+                if (inv.getItem(0) == null || inv.getItem(0).getType() == Material.AIR ||
+                    inv.getItem(1) == null || inv.getItem(1).getType() == Material.AIR ||
+                    inv.getItem(2) == null || inv.getItem(2).getType() == Material.AIR) {
+                        return;
+                }
+                final Material mat = inv.getItem(2).getType();
+                final Player player = (Player)event.getWhoClicked();
+                final int oldLevel = player.getLevel();
+
+                new BukkitRunnable() {
+                        public void run() {
+                                final int xpLevel = Math.min(39, oldLevel - player.getLevel()) / 2;
+                                if (xpLevel > 0) onEnchant(player, mat, xpLevel);
+                        }
+                }.runTask(plugin);
+        }
+
+        public void onEnchant(Player player, Material mat, int xpLevels) {
                 // Give SP.
-                final int sp = itemSpMap.roll(item.getType(), xpLevels);
+                final int sp = itemSpMap.roll(mat, xpLevels);
                 addSkillPoints(player, sp);
         }
 
